26f8410b9a4a922894bd6ca8bf9bc8255605e9e6
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-11-06 18:32:24 +0200

    up

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; protected set; }
+        public string FavoriteFood { get; protected set; }
+        public Animal(string name, string favFood)
+        {
+            Name = name;
+            FavoriteFood = favFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavoriteFood}";
+        }
+    }
+}
