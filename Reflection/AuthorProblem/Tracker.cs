c6dcd2123ca5abc768d154c7604b686796c843ab
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-11-17 21:10:18 +0200

    Add files via upload

---
+++ Reflection/AuthorProblem/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            var getMethods = type.GetMethods(BindingFlags.Public|BindingFlags.Static|BindingFlags.Instance);
+            foreach (var item in getMethods)
+            {
+                if (item.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = item.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine("{0} is written by {1}", item.Name, attr.Name);
+                    }
+                }
+            }
+        }
+    }
+}
