93adf34c8da9a1d9137a9c417105c3f42b102935
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2022-12-05 18:07:01 +0200

    Add files via upload

---
+++ BookSkeleton/Book.Tests/Tests.cs
@@ -0,0 +1,99 @@
+﻿namespace Book.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+    using static System.Net.Mime.MediaTypeNames;
+
+    public class Tests
+    {
+        [Test]
+        public void BookNameIsValid()
+        {
+            const string name = "Dido";
+            const string author = "Dido1";
+            Book book = new Book(name, author);
+            Assert.AreEqual(name, book.BookName);
+        }
+        [Test]
+        public void BookNameIsInvalidWithNull()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book(null, "Author");
+            });
+        }
+        [Test]
+        public void BookNameIsEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book(String.Empty, "Author");
+            });
+        }
+
+        [Test]
+        public void AuthorShouldBeValid()
+        {
+            const string name = "Dido";
+            const string author = "Dido1";
+            Book book = new Book(name, author);
+            Assert.AreEqual(author, book.Author);
+        }
+        [Test]
+        public void AuthorShouldBeNull()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book("Name", null);
+            });
+        }
+        [Test]
+        public void AuthorShouldBeEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book("Name", String.Empty);
+            });
+        }
+
+        [Test]
+        public void AddFootNoteSouldWork()
+        {
+            Book book = new Book("Name", "Author");
+            book.AddFootnote(1, "Text");
+            book.AddFootnote(2, "Text1");
+            Assert.AreEqual(2, book.FootnoteCount);
+        }
+        [Test]
+        public void AddFootNoteShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var book = new Book("Name", "Author");
+                book.AddFootnote(1, "Text");
+                book.AddFootnote(1, "Text1");
+            });
+        }
+
+        [Test]
+        public void FindFootnoteShouldWork()
+        {
+            Book book = new Book("Name", "Author");
+            book.AddFootnote(1, "Text");
+            var footnote = book.FindFootnote(1);
+            Assert.AreEqual("Footnote #1: Text", footnote);
+        }
+        [Test]
+        public void FindFootnoteShouldNotWork()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Book book = new Book("Name", "Author");
+                book.AddFootnote(1, "Text");
+                var footnote = book.FindFootnote(2);
+                Assert.AreEqual("Footnote #1: Text", footnote);
+            });
+        }
+    }
+}
